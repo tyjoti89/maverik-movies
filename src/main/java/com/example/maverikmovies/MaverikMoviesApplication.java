a597ed12a4d36414252632a33396380e4905934e
Tyler Tippetts <tyjoti89@gmail.com>
2023-07-21 15:54:30 -0600

    first commit

---
+++ src/main/java/com/example/maverikmovies/MaverikMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.example.maverikmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaverikMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaverikMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maverikmovies/MaverikMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maverikmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaverikMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
